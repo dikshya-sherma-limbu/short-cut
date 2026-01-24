04c879ecf35ef21cf3be9c85ee09085c5f0bdf37
Dikshya  Sherma Limbu <dikshyashermalimbu2233@gmail.com>
2026-01-24 16:06:25 -0500

    finally worked fully - from frontend to backend - just the result data need to fix like i am getting the object object - so i need to change the format on backend to get proper result data to show in UI in frontend

--- services/graphService.js
+++ services/graphService.js
@@ -4,17 +4,30 @@ import axios from "axios";
 // call the graph api to get the shortest path between two locations
 export const getShortestPath = async (origin, destination, travelMode) => {
   try {
-    console.log("Fetching shortest path with params:", { origin, destination, travelMode });
-    const response = await axios.get("http://192.168.5.153:5000/transit/shortest-route", {
-        params: {
-            origin: JSON.stringify(origin),
-            destination: JSON.stringify(destination),
-            travelMode
-        }
+    console.log("Fetching shortest path with params:", {
+      origin,
+      destination,
+      travelMode,
     });
+    const response = await axios.get(
+      "http://192.168.5.173:5000/transit/shortest-route",
+      {
+        params: {
+          origin: JSON.stringify(origin),
+          destination: JSON.stringify(destination),
+          travelMode,
+        },
+      },
+    );
     return response.data;
   } catch (error) {
-    console.error("Error fetching shortest path:", error, error.response?.data, error.message, error.axiosError);
+    console.error(
+      "Error fetching shortest path:",
+      error,
+      error.response?.data,
+      error.message,
+      error.axiosError,
+    );
     throw error;
-  } 
+  }
 };
